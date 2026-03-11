c5449b2822cdff58a0043e0239dc1c4332318edb
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-03-11 22:54:47 +0500

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './Pages/Profile';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
-      
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue(), svelte(), etc.
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true, // This is the magic line that fixes the "no-refresh" bug
+    },
+  },
 })
