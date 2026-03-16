7f7e46c5abd79407ed4af68386b05d9c3c18c9dc
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-03-16 10:35:09 +0500

    handle Sign-up functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,27 +2,13 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import userModel from "../models/user.model.js";
 
-export const signUp = async(req,res) =>{
+export const signUp = async(req,res,next) =>{
 
     const {username,email,password} =  req.body;
-
-    if(!username||!email||!password){
-        return res.json ({
-            success:false,
-            message : 'missing Details'
-        })
-    }
 
     try{
 
         const existingUser = await userModel.findOne({email})
-
-        if(existingUser){
-            return res.json ({
-            success:false,
-            message : 'User Already exist!'
-        })
-        }
 
         //if user not exist then store password
         const hasPassword = await bcrypt.hash(password,10);
@@ -31,26 +17,12 @@ export const signUp = async(req,res) =>{
         const user = new userModel({username,email,password: hasPassword})
 
         await user.save()
-
-       
-        
-      
-
        
         return res.json({ success: true });
 
 
     }
     catch(error){
-        return res.json ({
-            success:false,
-            message : error.message
-        })
+        next(error);
     }
-
-
-
-
-
-
 }
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
 
-function SignUp() {
+export default function SignUp() {
+
+  
   return (
-    <div>
-      <h1>sigup</h1>
-    </div>
-  )
-}
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form  className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+        />
 
-export default SignUp
+        <button className="text-white bg-slate-700 p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Signup
+        </button>
+       
+      </form>
+      
+      
+    </div>
+  );
+}
