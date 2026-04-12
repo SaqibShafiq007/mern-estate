84cb240d90b19c388919ddf74d206ea06cf4d3b1
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-04-12 18:08:00 +0500

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store.js'
+import { persistor,store } from './redux/store.js'
 import { Provider } from 'react-redux'//the wrapper that GIVES store access to all components
-
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 )
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,59 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  userReducer from './user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+//we have many reducers (user, products, cart, etc.)
+//Redux needs one single reducer — so combineReducers merges them all into one big object
+const rootReducer = combineReducers(
+  {
+    user:userReducer
+  }
+);
+
+
+//how to save data
+const persistConfig = {
+  key:'root',
+  storage,
+  version:1,
+};
+
+//User logs in → saved in RAM → ALSO saved in localStorage → refresh → loaded back from localStorage → STILL THERE ✅
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+
 
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 })
+
+
+export const persistor = persistStore(store);
+
+
+
+/*
+
+App Starts
+    ↓
+PersistGate checks localStorage
+    ↓
+Found saved data? → Load into Redux Store
+    ↓
+App renders with previous state (user still logged in!) ✅
+
+User does something (login/logout)
+    ↓
+Redux Store updates
+    ↓
+persistReducer AUTO-SAVES to localStorage 💾
+
+Page Refreshes → cycle repeats ♻️
+
+*/
