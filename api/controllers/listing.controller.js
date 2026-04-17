df962252c69c84399ad9cacd66a799026c31db09
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-04-17 16:48:04 +0500

    create listing api route

---
+++ api/controllers/listing.controller.js
@@ -0,0 +1,12 @@
+import Listing from "../models/listing.model.js";
+
+
+// runs when someone sends a request to create a new property listing
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
