2da21b9033f45ac0b56757a4848839ab24d5ca78
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-04-17 18:33:51 +0500

    create Listing pg UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,7 @@ import Header from './components/Header';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIN';
 import PrivateRoute from './components/PrivateRoute';
-
-
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -23,6 +22,7 @@ export default function App() {
 
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Route>
 
       </Routes>
