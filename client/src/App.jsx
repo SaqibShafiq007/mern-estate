50e91c9101133b8b0f34060f0e119ed53d96bfd6
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-04-20 22:07:31 +0500

    create Search page ui and events

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/updateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/listing/:id' element={<Listing />} />
+        <Route path='/search' element={<Search />} />
         
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
