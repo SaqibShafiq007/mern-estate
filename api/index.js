900f68efc48b55c9bf9c9eaab84e2a5df038a31c
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-04-21 23:40:16 +0500

    add pre-installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
+
 dotenv.config()
 mongoose
   .connect(process.env.MONGO)
@@ -16,13 +18,28 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json()); 
 app.use(cookieParser())
+
+
+app.listen(4000, ()=>{
+    console.log("app is running on port 4000")
+})
+
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   
@@ -36,8 +53,3 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-
-app.listen(4000, ()=>{
-    console.log("app is running on port 4000")
-})
